e07b0f966203bbd25d0112813ba9d2ac4ed33151
marwa <marwajawad19@gmail.com>
2021-09-09 18:05:31 +0300

    import whoseHouse and gryffFunctions

--- src/Hogwarts.js
+++ src/Hogwarts.js
@@ -1,23 +1,13 @@
+// src/Hogwarts.js
 import React from "react";
-import whoseHouse from "./houses/whoseHouse";
-import Hufflepuff from "./houses/Hufflepuff";
+import whoseHouse from "./houses/HagridsHouse";
+import HooflePoof from "./houses/Hufflepuff";
 import * as GryffFunctions from "./houses/Gryffindor";
 
-console.log(GryffFunctions.colors);
-// => 'Scarlet and Gold'
-
-GryffFunctions.gryffMascot();
-// => 'The Lion'
-
-// GryffFunctions.values();
-// => Attempted import error
-
 export default function Hogwarts() {
-  whoseHouse();
-
   return (
     <div>
-      <Hufflepuff />
+      <HooflePoof />
     </div>
   );
 }
